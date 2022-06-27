5521467fd12f5f25139d210d13b5ba542ac5b139
Julia SM <cienvariables@gmail.com>
2022-06-28 00:27:18 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,2 @@
+<?php echo "hola mundo, soy Julia";
+// La manera más sencilla de instalar PHP en Windows es con un instalador con ciertas aplicaciones ya preparadas para el desarrollo. Uno de ellos es XAMPP de los más famosos entornos de desarrollo, que include además del intérprete PHP, un servidor Web (Apache) y una base de datos (MySQL).
